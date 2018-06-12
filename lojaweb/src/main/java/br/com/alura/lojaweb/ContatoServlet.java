a2c5f4637727650cb40371b1924d73aa1384b821
anderson <anderson.bestteti@gmail.com>
2018-06-12 00:03:25 -0300

    MAVEN - finalizacao do curso.

--- lojaweb/src/main/java/br/com/alura/lojaweb/ContatoServlet.java
+++ lojaweb/src/main/java/br/com/alura/lojaweb/ContatoServlet.java
@@ -9,11 +9,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import br.com.alura.maven.produtos.Produto;
+
 @WebServlet(urlPatterns="/contato")
 public class ContatoServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		 PrintWriter writer = resp.getWriter();
+		 Produto p = new Produto("bala xaxa", 0.15);
 		 writer.println("<html><h2>Fale conosco!</h2></html>");
 		 writer.close();
 	}
