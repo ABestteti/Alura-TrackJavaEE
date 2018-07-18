d2e0440922958c3a22fdbaffcbce204c3c58ad8f
anderson.bestteti@gmail.com <anderson.bestteti@gmail.com>
2018-07-17 23:52:23 -0300

    Conversao e validacao de dados no JFS

--- jfs-livraria/src/br/com/caelum/livraria/bean/LivroBean.java
+++ jfs-livraria/src/br/com/caelum/livraria/bean/LivroBean.java
@@ -2,12 +2,16 @@ package br.com.caelum.livraria.bean;
 
 import java.util.List;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
 
 import br.com.caelum.livraria.dao.DAO;
 import br.com.caelum.livraria.modelo.Autor;
 import br.com.caelum.livraria.modelo.Livro;
+import sun.security.validator.ValidatorException;
 
 @ManagedBean
 @ViewScoped
@@ -15,11 +19,11 @@ public class LivroBean {
 
 	private Livro livro = new Livro();
 	private Integer autorId;
-	
+
 	public void setAutorId(Integer autorId) {
 		this.autorId = autorId;
 	}
-	
+
 	public Integer getAutorId() {
 		return autorId;
 	}
@@ -27,7 +31,7 @@ public class LivroBean {
 	public List<Livro> getLivros() {
 		return new DAO<Livro>(Livro.class).listaTodos();
 	}
-	
+
 	public Livro getLivro() {
 		return livro;
 	}
@@ -35,7 +39,7 @@ public class LivroBean {
 	public List<Autor> getAutores() {
 		return new DAO<Autor>(Autor.class).listaTodos();
 	}
-	
+
 	public List<Autor> getAutoresDoLivro() {
 		return this.getAutores();
 	}
@@ -45,19 +49,28 @@ public class LivroBean {
 		this.livro.adicionaAutor(autor);
 		System.out.println("Livro escrito por: " + autor.getNome());
 	}
-	
+
 	public void gravar() {
 		System.out.println("Gravando livro " + this.livro.getTitulo());
 
 		if (livro.getAutores().isEmpty()) {
-			throw new RuntimeException("Livro deve ter pelo menos um Autor.");
+			//throw new RuntimeException("Livro deve ter pelo menos um Autor.");
+			FacesContext.getCurrentInstance().addMessage("autor", new FacesMessage("Livro deve ter pelo menos um Autor."));
+			return;
 		}
 
 		new DAO<Livro>(Livro.class).adiciona(this.livro);
-		
+
 		// Create a new Livro instance in order to clean the page
 		// after Gravar action has been done.
 		this.livro = new Livro();
 	}
 
+	public void comecaComDigitoUm(FacesContext fc, UIComponent component, Object value) throws ValidatorException {
+		String valor = value.toString();
+
+		if (!valor.startsWith("1")) {
+			throw new ValidatorException(new FacesMessage("Deveria começar com 1"));
+		}
+	}
 }
