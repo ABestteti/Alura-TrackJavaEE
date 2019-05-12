efc93fdee731c9e1c4309f1d449f0d3050856ffb
anderson <anderson.bestteti@gmail.com>
2019-05-12 20:42:51 -0300

    EntityManager and Dao classes implementation.

--- auron/src/main/java/org/auron/beans/ParticipanteBean.java
+++ auron/src/main/java/org/auron/beans/ParticipanteBean.java
@@ -1,17 +1,24 @@
 package org.auron.beans;
 
 import javax.faces.bean.RequestScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 
+import org.auron.dao.ParticipanteDao;
 import org.auron.modelo.Participante;
 
 @Named
 @RequestScoped
 public class ParticipanteBean {
+	
+	@Inject
+	private ParticipanteDao participanteDao;
+	
 	private Participante participante = new Participante();
 	
 	public void cadastrar() {
 		System.out.println(participante.getNome());
+		participanteDao.inserir(participante);
 	}
 
 	public Participante getParticipante() {
--- auron/src/main/java/org/auron/dao/ParticipanteDao.java
+++ auron/src/main/java/org/auron/dao/ParticipanteDao.java
@@ -4,11 +4,15 @@ import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.auron.modelo.Participante;
+
 @Stateless
 public class ParticipanteDao {
 
 	@PersistenceContext
 	private EntityManager em;
 	
-	
+	public void inserir(Participante participante) {
+		em.persist(participante);
+	}
 }
