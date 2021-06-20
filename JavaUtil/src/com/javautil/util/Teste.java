654a1c1588d9142bbac4f034ec50c0da69c5e32e
Anderson Bestteti <anderson.bestteti@gmail.com>
2021-06-20 17:24:44 -0300

    Comparator implementation

--- JavaUtil/src/com/javautil/util/Teste.java
+++ JavaUtil/src/com/javautil/util/Teste.java
@@ -16,12 +16,30 @@ public class Teste {
 	        aulas.add(aulaIntro);
 	        aulas.add(aulaModelando);
 	        aulas.add(aulaSets);
+	        aulas.add(new Aula("Banco de dados", 21));
+	        aulas.add(new Aula("Compiladores", 25));
 
 	        Collections.sort(aulas);
 
 	        for (Aula aula : aulas) {
 	            System.out.println(aula);
 	        }
+
+	        
+	        System.out.println("------ Reverse --------");
+	        Collections.reverse(aulas);
+
+	        for (Aula aula : aulas) {
+	            System.out.println(aula);
+	        }
+
+	        System.out.println("------ Roated --------");
+	        Collections.rotate(aulas, 2);
+
+	        for (Aula aula : aulas) {
+	            System.out.println(aula);
+	        }
+
 	    }
 	}
 
