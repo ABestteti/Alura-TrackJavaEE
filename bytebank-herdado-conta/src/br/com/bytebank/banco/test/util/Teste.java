e690cb97e1d077c86c3c8428822da30c3d0bd974
Anderson Bestteti <anderson.bestteti@gmail.com>
2021-06-20 18:21:54 -0300

    Optmized comparator

--- bytebank-herdado-conta/src/br/com/bytebank/banco/test/util/Teste.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/test/util/Teste.java
@@ -67,9 +67,7 @@ class TitularDaContaComparator implements Comparator<Conta> {
 
 	@Override
 	public int compare(Conta c1, Conta c2) {
-		String nomeC1 = c1.getTitular().getNome();
-		String nomeC2 = c2.getTitular().getNome();
-		return nomeC1.compareTo(nomeC2);
+		return c1.getTitular().getNome().compareTo(c2.getTitular().getNome());
 	}
 }
 
